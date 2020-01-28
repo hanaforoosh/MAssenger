5f0e934aaff217747face64d35f48c732b44d164
Mahrad <mahrad.hanaforoosh@yahoo.com>
2020-01-28 19:22:16 +0330

    Final Inital :)

--- MAssenger/MAssenger/App_Start/WebApiConfig.cs
+++ MAssenger/MAssenger/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace MAssenger
@@ -16,9 +17,17 @@ namespace MAssenger
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            //config.Routes.MapHttpRoute(
+            //    name: "SecondApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
