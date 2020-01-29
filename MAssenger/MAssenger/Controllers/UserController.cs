86b39f166a1c5a3ba3dfa51f749b701aebd16c52
Mahrad <mahrad.hanaforoosh@yahoo.com>
2020-01-29 12:11:23 +0330

    Request Class

--- MAssenger/MAssenger/Controllers/UserController.cs
+++ MAssenger/MAssenger/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,10 +10,19 @@ namespace MAssenger.Controllers
 {
     public class UserController : ApiController
     {
+        private List<User> users
 
-        public string Get()
-        {
-            return "Hello!";
+        [HttpGet]
+        public IHttpActionResult Get([FromUri] Request req)
+        {  
+            return Ok(req.GetContent<Test>());
         }
+
+    }
+
+    public class Test
+    {
+        private int x;
+        public string Mystring { get; set; }
     }
 }
