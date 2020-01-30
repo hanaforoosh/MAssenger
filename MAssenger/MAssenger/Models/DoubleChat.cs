deb1e14dc041bf8fe419d2eedce526916e1c47fe
Mahrad <mahrad.hanaforoosh@gmailcom>
2020-01-31 01:02:33 +0330

    changed again

---
+++ MAssenger/MAssenger/Models/DoubleChat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace MAssenger
+{
+    public class DoubleChat : Conversation
+    {
+        public ICollection<Message> StarMessages = new List<Message>();
+    }
+}
