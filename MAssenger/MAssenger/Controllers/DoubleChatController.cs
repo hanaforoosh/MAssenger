f2f284fce07bcabcfe10fae446a6c674574f70bf
Mahrad <mahrad.hanaforoosh@gmailcom>
2020-02-01 00:55:51 +0330

    Double Chat

--- MAssenger/MAssenger/Controllers/DoubleChatController.cs
+++ MAssenger/MAssenger/Controllers/DoubleChatController.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using MAssenger.DAL;
+using MAssenger.Models;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,12 +15,22 @@ namespace MAssenger.Controllers
         [HttpGet]
         public IHttpActionResult MarkMessageInChat([FromBody] JObject request)
         {
+            Message msg = request["message"].ToObject<Message>();
+            Repo<DoubleChat> cr = new DoubleChatRepo();
+            DoubleChat chat = request["chat"].ToObject<DoubleChat>();
+            chat.MarkMessage(msg);
+            cr.Update(chat);
             return Ok();
         }
 
         [HttpGet]
         public IHttpActionResult UnMarkMessageInChat([FromBody] JObject request)
         {
+            Message msg = request["message"].ToObject<Message>();
+            Repo<DoubleChat> cr = new DoubleChatRepo();
+            DoubleChat chat = request["chat"].ToObject<DoubleChat>();
+            chat.UnMarkMessage(msg);
+            cr.Update(chat);
             return Ok();
         }
     }
