2b46d8d84c6ccdc3ce587262d1e1ce44e02ce155
Mahrad <mahrad.hanaforoosh@gmailcom>
2020-02-01 01:09:40 +0330

    Group Controller

--- MAssenger/MAssenger/Controllers/GroupController.cs
+++ MAssenger/MAssenger/Controllers/GroupController.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using MAssenger.DAL;
+using MAssenger.Models;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,27 +15,51 @@ namespace MAssenger.Controllers
         [HttpGet]
         public IHttpActionResult SelectAdmin([FromBody] JObject request)
         {
+            User conv = request["user"].ToObject<User>();
+            Repo<Group> cr = new GroupRepo();
+            Group group = request["group"].ToObject<Group>();
+            group.Admin = conv;
+            cr.Update(group);
             return Ok();
         }
 
         [HttpGet]
         public IHttpActionResult ChangeAdmin([FromBody] JObject request)
         {
+            User conv = request["user"].ToObject<User>();
+            Repo<Group> cr = new GroupRepo();
+            Group group = request["group"].ToObject<Group>();
+            group.Admin = conv;
+            cr.Update(group);
             return Ok();
         }
         [HttpGet]
         public IHttpActionResult UnsetAdmin([FromBody] JObject request)
         {
+            Repo<Group> cr = new GroupRepo();
+            Group group = request.ToObject<Group>();
+            group.Admin = null;
+            cr.Update(group);
             return Ok();
         }
         [HttpGet]
         public IHttpActionResult PinMessage([FromBody] JObject request)
         {
+            Message msg = request["message"].ToObject<Message>();
+            Repo<Group> cr = new GroupRepo();
+            Group group = request["group"].ToObject<Group>();
+            group.PinnedMessage = msg;
+            cr.Update(group);
             return Ok();
         }
         [HttpGet]
         public IHttpActionResult UnPinMessage([FromBody] JObject request)
         {
+            Message msg = request["message"].ToObject<Message>();
+            Repo<Group> cr = new GroupRepo();
+            Group group = request["group"].ToObject<Group>();
+            group.PinnedMessage = null;
+            cr.Update(group);
             return Ok();
         }
 
---
+++ MAssenger/MAssenger/DAL/GroupRepo.cs
@@ -0,0 +1,43 @@
+﻿using MAssenger.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MAssenger.DAL
+{
+    public class GroupRepo : Repo<Group>
+
+    {
+        public GroupRepo() : base(new DBMySQL()) { }
+        public override Group Create(Group entity)
+        {
+            return entity;
+        }
+
+        public override bool Delete(Group entity)
+        {
+            return true;
+        }
+
+        public override bool Delete(AModel id)
+        {
+            return true;
+        }
+
+        public override Group Read(AModel id)
+        {
+            return new Group();
+        }
+
+        public override ICollection<Group> ReadAll()
+        {
+            return new List<Group>();
+        }
+
+        public override Group Update(Group entity)
+        {
+            return entity;
+        }
+    }
+}
