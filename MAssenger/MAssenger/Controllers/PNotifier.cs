8d4dcdf9e0ac7c6b78c491707c49e0df6f6e2ac7
Mahrad <mahrad.hanaforoosh@gmailcom>
2020-01-31 16:19:01 +0330

    semi final 2

---
+++ MAssenger/MAssenger/Controllers/PNotifier.cs
@@ -0,0 +1,19 @@
+﻿using MAssenger.Models;
+
+namespace MAssenger.Controllers
+{
+    internal class PNotifier : IPushNotifier
+    {
+        public void Broadcast(Message m)
+        {
+        }
+
+        public void notify(Message m)
+        {
+        }
+
+        public void ResendNotification(Message m)
+        {
+        }
+    }
+}
---
+++ MAssenger/MAssenger/Controllers/ProfileController.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MAssenger.Controllers
+{
+    public class ProfileController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult GetBio([FromBody] JObject request)
+        {
+            return Ok();
+        }
+        public IHttpActionResult SetBio([FromBody] JObject request)
+        {
+            return Ok();
+        }
+        public IHttpActionResult GetAvatar([FromBody] JObject request)
+        {
+            return Ok();
+        }
+        public IHttpActionResult SetAvatar([FromBody] JObject request)
+        {
+            return Ok();
+        }
+        public IHttpActionResult SetPhonenumber([FromBody] JObject request)
+        {
+            return Ok();
+        }
+        public IHttpActionResult GetPhonenumber([FromBody] JObject request)
+        {
+            return Ok();
+        }
+    }
+}
